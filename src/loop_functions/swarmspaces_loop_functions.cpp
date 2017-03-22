4df08f411171006a1b04a51a6b15950a21bf4743
VishaalMK <mkvishaal@gmail.com>
2017-03-22 19:50:20 -0400

    New branch for loop functions, debug

---
+++ src/loop_functions/swarmspaces_loop_functions.cpp
@@ -0,0 +1,277 @@
+#include "swarmspaces_loop_functions.h"
+#include "ck_footbot_diffusion.h"
+#include "SwarmSpaces.h"
+#include <swarmspaces/swarmtuple.cpp>
+
+#include <argos3/plugins/simulator/entities/box_entity.h>
+#include <argos3/plugins/robots/foot-bot/simulator/footbot_entity.h>
+
+#include <sstream>
+#include <list>
+
+/****************************************/
+/****************************************/
+
+static const std::string FILE_PREFIX      = "space_";
+static const Real        FB_RADIUS        = 0.085036758f;
+static const Real        FB_AREA          = ARGOS_PI * Square(0.085036758f);
+static const std::string FB_CONTROLLER    = "fdc";
+//static const UInt32      MAX_PLACE_TRIALS = 20;
+//static const UInt32      MAX_ROBOT_TRIALS = 20;
+static const Real        RAB_RANGE        = 3.0f;
+static const Real        SF_RANGE         = RAB_RANGE / Sqrt(2);
+static const Real        HALF_SF_RANGE    = SF_RANGE * 0.5f;
+static const Real        WALL_THICKNESS   = 0.1;
+static const Real        WALL_HEIGHT      = 2.0;
+
+// Tuple Created
+// Or can this be done in the header file??
+CSwarmTuple tuple(1, CVector2(0.0, 0.0), 0.5, "First Tuple!");
+
+
+/****************************************/
+/****************************************/
+
+CSwarmSpacesLF::CSwarmSpacesLF() :
+   m_bDone(false),
+   isCheckValid(false),
+   tupleCount(0){
+}
+
+/****************************************/
+/****************************************/
+
+CSwarmSpacesLF::~CSwarmSpacesLF() {
+}
+
+/**
+    * Varies the size of Arena based on density using PlaceWalls()
+    * Distributes robots using PlaceUniformly()
+    * Spawns the tuple in the environment
+    */
+void CSwarmSpacesLF::Init(TConfigurationNode& t_tree) {
+   LOG.Flush();
+   LOGERR.Flush();
+   try {
+      /* Parse the configuration file */
+      GetNodeAttribute(t_tree, "outfile", m_strOutFile);
+      UInt32 unRobots;
+      GetNodeAttribute(t_tree, "robots", unRobots);
+      UInt32 unDataSize;
+      GetNodeAttribute(t_tree, "data_size", unDataSize);
+      Real fDensity;
+      GetNodeAttribute(t_tree, "density", fDensity);
+
+      PlaceWalls(unRobots, unDataSize, fDensity);
+
+// Iterates through all the robots and the first one in range of the SwarmTuple spawns it
+      CSpace::TMapPerType& m_cFootbots = GetSpace().GetEntitiesByType("foot-bot");
+      bool isSpawned = false;
+      for(CSpace::TMapPerType::iterator it = m_cFootbots.begin();
+           it != m_cFootbots.end();
+           ++it) {
+            /* Get handle to foot-bot entity and controller */
+            CFootBotEntity& cFootBot = *any_cast<CFootBotEntity*>(it->second);
+            CFootBotForaging& cController = dynamic_cast<CFootBotForaging&>(cFootBot.GetControllableEntity().GetController());
+            if(isSpawned == false){
+            	if(cController.InTupleRange(tuple)){
+            		cController.swarmSpace.write(tuple);
+            		isSpawned = true;
+            		break;
+            	}
+            }
+      }
+      /* Initialize the rest */
+      Reset();
+   }
+   catch(CARGoSException& ex) {
+      THROW_ARGOSEXCEPTION_NESTED("Error initializing the loop functions", ex);
+   }
+}
+
+/****************************************/
+/****************************************/
+
+void CSwarmSpacesLF::PreStep() {
+
+}
+
+
+/**
+    * Writes the current time and the number of
+    * tuples alive in the swarm to the output file
+    *
+    * isCheckValid becomes true after the number of
+    * live tuples in the swarm becomes atleast 1
+    */
+void CSwarmSpacesLF::PostStep() {
+	CSpace::TMapPerType& m_cFootbots = GetSpace().GetEntitiesByType("foot-bot");
+	for(CSpace::TMapPerType::iterator it = m_cFootbots.begin();
+	           it != m_cFootbots.end();
+	           ++it) {
+	            /* Get handle to foot-bot entity and controller */
+	            CFootBotEntity& cFootBot = *any_cast<CFootBotEntity*>(it->second);
+	            CFootBotForaging& cController = dynamic_cast<CFootBotForaging&>(cFootBot.GetControllableEntity().GetController());
+	            size_t aliveCount = cController.swarmSpace.size();
+	            tupleCount += aliveCount;
+    }
+	if(tupleCount >= 1){
+		isCheckValid = true;
+	}
+	m_cOutput << GetSpace().GetSimulationClock() << "\t"
+	          << tupleCount << std::endl;
+}
+
+/****************************************/
+/****************************************/
+
+void CSwarmSpacesLF::Reset() {
+   OpenFile(m_cOutFile, FILE_PREFIX);
+}
+
+/****************************************/
+/****************************************/
+
+void CSwarmSpacesLF::Destroy() {
+   CloseFile(m_cOutFile);
+}
+
+/**
+    * The experiment id finished if the number of tuples alive in
+    * the swarm goes to zero
+    *
+    * The zero at the start of the process is accounted for using isCheckValid
+    */
+
+bool CSwarmSpacesLF::IsExperimentFinished() {
+	if(isCheckValid == true && tupleCount == 0){
+		m_bDone = true;
+	}
+	return m_bDone;
+}
+
+/****************************************/
+/****************************************/
+
+void CSwarmSpacesLF::PlaceWalls(UInt32 un_robots,
+                        UInt32 un_data_size,
+                        Real f_density) {
+   /* Calculate arena side */
+   Real fArenaSide =
+      RAB_RANGE *
+      Sqrt((25.0 * ARGOS_PI * un_robots) /
+           ((100.0 - 4.0 * ARGOS_PI) * f_density));
+   Real fArenaSide2 = fArenaSide / 2.0;
+   Real fArenaSide5 = fArenaSide / 5.0;
+   Real fArenaSide10 = fArenaSide / 10.0;
+   /* Place the north wall */
+   AddEntity(
+      *new CBoxEntity("wall_north",
+                      CVector3(fArenaSide2, 0, 0),
+                      CQuaternion(),
+                      false,
+                      CVector3(WALL_THICKNESS, fArenaSide, WALL_HEIGHT)));
+   /* Place the south wall */
+   AddEntity(
+      *new CBoxEntity("wall_south",
+                      CVector3(-fArenaSide2, 0, 0),
+                      CQuaternion(),
+                      false,
+                      CVector3(WALL_THICKNESS, fArenaSide, WALL_HEIGHT)));
+   /* Place the west wall */
+   AddEntity(
+      *new CBoxEntity("wall_west",
+                      CVector3(0, fArenaSide2, 0),
+                      CQuaternion(),
+                      false,
+                      CVector3(fArenaSide, WALL_THICKNESS, WALL_HEIGHT)));
+   /* Place the east wall */
+   AddEntity(
+      *new CBoxEntity("wall_east",
+                      CVector3(0, -fArenaSide2, 0),
+                      CQuaternion(),
+                      false,
+                      CVector3(fArenaSide, WALL_THICKNESS, WALL_HEIGHT)));
+
+   /* Calculate side of the region in which the robots are scattered */
+   CRange<Real> cAreaRange(-fArenaSide2, fArenaSide2);
+   /* Place robots */
+   PlaceUniformly(un_robots, un_data_size, cAreaRange);
+}
+
+/****************************************/
+/****************************************/
+
+void CSwarmSpacesLF::PlaceUniformly(UInt32 un_robots,
+                            UInt32 un_data_size,
+                            CRange<Real> c_area_range) {
+   UInt32 unTrials;
+   CFootBotEntity* pcFB;
+   std::ostringstream cFBId;
+   CVector3 cFBPos;
+   CQuaternion cFBRot;
+   /* Create a RNG (it is automatically disposed of by ARGoS) */
+   CRandom::CRNG* pcRNG = CRandom::CreateRNG("argos");
+   /* For each robot */
+   for(size_t i = 0; i < un_robots; ++i) {
+      /* Make the id */
+      cFBId.str("");
+      cFBId << "fb" << i;
+      /* Create the robot in the origin and add it to ARGoS space */
+      pcFB = new CFootBotEntity(
+         cFBId.str(),
+         FB_CONTROLLER,
+         CVector3(),
+         CQuaternion(),
+         RAB_RANGE,
+         un_data_size);
+      AddEntity(*pcFB);
+      /* Add its controller to the list */
+      m_vecControllers.push_back(
+         &dynamic_cast<CBuzzControllerFootBot&>(
+            pcFB->GetControllableEntity().GetController()));
+      /* Try to place it in the arena */
+      unTrials = 0;
+      bool bDone;
+      do {
+         /* Choose a random position */
+         ++unTrials;
+         cFBPos.Set(pcRNG->Uniform(c_area_range),
+                    pcRNG->Uniform(c_area_range),
+                    0.0f);
+         cFBRot.FromAngleAxis(pcRNG->Uniform(CRadians::UNSIGNED_RANGE),
+                              CVector3::Z);
+         bDone = MoveEntity(pcFB->GetEmbodiedEntity(), cFBPos, cFBRot);
+      } while(!bDone && unTrials <= MAX_PLACE_TRIALS);
+      if(!bDone) {
+         THROW_ARGOSEXCEPTION("Can't place " << cFBId.str());
+      }
+   }
+}
+
+/****************************************/
+/****************************************/
+
+void CSwarmSpacesLF::OpenFile(std::ofstream& c_stream,
+                      const std::string& str_prefix) {
+   /* Make filename */
+   std::string strFName = str_prefix + m_strOutFile;
+   /* Close file and reopen it */
+   CloseFile(c_stream);
+   c_stream.open(strFName.c_str(),
+                 std::ofstream::out | std::ofstream::trunc);
+   if(c_stream.fail())
+      THROW_ARGOSEXCEPTION("Error opening \"" << strFName << "\": " << strerror(errno));
+}
+
+/****************************************/
+/****************************************/
+
+void CSwarmSpacesLF::CloseFile(std::ofstream& c_stream) {
+   if(c_stream.is_open()) c_stream.close();
+}
+
+/****************************************/
+/****************************************/
+
+REGISTER_LOOP_FUNCTIONS(CSwarmSpacesLF, "swarmspaces_loop_functions");
---
+++ src/loop_functions/swarmspaces_loop_functions.h
@@ -0,0 +1,49 @@
+
+#ifndef SWARMSPACES_LOOP_FUNCTIONS_H
+#define SWARMSPACES_LOOP_FUNCTIONS_H
+
+#include <argos/core/simultor/loop_functions.h>
+#include <fstream>
+#include <string>
+
+using namespace argos;
+
+class CSwarmSpacesLF : public CLoopFunctions {
+
+public:
+
+	CSwarmSpacesLF();
+	virtual ~CSwarmSpacesLF();
+
+	virtual void Init(TConfigurationNode& t_tree);
+	virtual void PostStep();
+	virtual void Reset();
+	virtual void Destroy();
+	virtual void PreStep();
+	virtual bool IsExperimentFinished();
+
+private:
+
+	void PlaceWalls(UInt32 un_robots,
+	                   UInt32 un_data_size,
+	                   Real f_distance);
+
+	void PlaceUniformly(UInt32 un_robots,
+	                       UInt32 un_data_size,
+	                       CRange<Real> c_area_range);
+	void OpenFile(std::ofstream& c_stream,
+	                 const std::string& str_prefix);
+	void CloseFile(std::ofstream& c_stream);
+
+private:
+
+	std::string m_strOutFile;
+	bool m_bDone;
+	bool isCheckValid;
+	size_t tupleCount;
+	std::ofstream m_cOutFile;
+
+};
+
+#endif
+
